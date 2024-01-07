d9312ecfce2bc6bb0738ffa3fbcde193a555a5a9
S4NKALP <sankalptharu50028@gmail.com>
2024-01-08 05:38:56 +0545

    Massive Updates

--- config/hypr/scripts/ClipManager.sh
+++ config/hypr/scripts/ClipManager.sh
@@ -2,8 +2,23 @@
 
 # Clipboard Manager. This needed cliphist & wl-copy and of course rofi
 
-if [[ ! $(pidof rofi) ]]; then
-	cliphist list | rofi -dmenu -config ~/.config/rofi/config-long.rasi | cliphist decode | wl-copy
-else
-	pkill rofi
-fi
+while true; do
+    if pidof rofi &> /dev/null; then
+        pkill rofi
+    fi
+
+    result=$(cliphist list | rofi -kb-custom-1 "Ctrl+Delete" -dmenu -config ~/.config/rofi/config-long.rasi)
+    exit_state=$?
+
+    [[ $exit_state -eq 1 ]] && break
+
+    case "$exit_state" in
+        0)
+            cliphist decode <<<"$result" | wl-copy
+            exit
+            ;;
+        10)
+            cliphist delete <<<"$result"
+            ;;
+    esac
+done
