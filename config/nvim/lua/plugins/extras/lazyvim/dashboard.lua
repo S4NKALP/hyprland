bb6973b12e096f18b5c3d1906b85177f1ccf1170
S4NKALP <sankalptharu50028@gmail.com>
2024-03-31 18:07:33 +0545

    nvim:update dashboard

--- config/nvim/lua/plugins/extras/lazyvim/dashboard.lua
+++ config/nvim/lua/plugins/extras/lazyvim/dashboard.lua
@@ -1,14 +1,78 @@
 return {
-	'nvimdev/dashboard-nvim',
-	dependencies = {
-		'kyazdani42/nvim-web-devicons'
-	},
-	enabled = true,
-	config = function()
-		-- local theme = require('plugins.dashboard-theme.landing')
-		-- local theme = require('plugins.dashboard-theme.minimal')
-		-- local theme = require('plugins.dashboard-theme.doom')
-		local theme = require('plugins.dashboard-theme.hyper')
-		require('dashboard').setup (theme)
-	end,
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            theme = 'hyper',
+            shortcut_type = 'letter',
+            config = {
+                header = { '',
+
+                ' ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+                ' ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+                ' ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+                ' ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+                ' ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+                ' ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+                '',
+                },
+                week_header = {
+                    enable = false
+                },
+                project = {
+                    enable = false,
+                    limit = 8,
+                    icon = ' ',
+                    group = '@function',
+                    label = 'Projects',
+                    action = 'Telescope find_files cwd='
+                },
+                mru = {
+                    limit = limit,
+                    icon = ' ',
+                    label = 'Recent (Last ' .. tostring(limit) .. ' files)',
+                },
+                shortcut = {
+                    {
+                        desc = '󰊳 Update',
+                        group = '@property',
+                        action = 'Lazy update',
+                        key = 'u'
+                    },
+                    {
+                        icon = ' ',
+                        icon_hl = '@variable',
+                        desc = 'Files',
+                        group = 'Label',
+                        action = 'Telescope find_files',
+                        key = 'f',
+                    },
+                    {
+                        icon = ' ',
+						            desc = 'New file',
+                        group = 'DiagnosticOk',
+                        action = 'ene',
+                        key = 'n'
+                    },
+                    {
+                        icon = ' ',
+                        desc = 'Words',
+                        group = 'DiagnosticWarn',
+                        action = 'Telescope live_grep',
+                        key = 'g',
+                    },
+                    {
+                        icon = ' ',
+                        desc = 'Quit',
+                        group = 'DiagnosticError',
+                        action = 'qa',
+                        key = 'q'
+                    },
+                },
+            },
+        }
+    end,
+    dependencies = {
+        {'nvim-tree/nvim-web-devicons'}
+    }
 }
