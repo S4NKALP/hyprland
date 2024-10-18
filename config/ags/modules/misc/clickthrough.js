23fd25b5593aee370520e995b9ea4ef4d7e390ba
S4NKALP <sankalptharu50028@gmail.com>
2024-10-18 09:57:15 +0545

    .

--- config/ags/modules/misc/clickthrough.js
+++
@@ -1,4 +0,0 @@
-import Cairo from "gi://cairo?version=1.0";
-
-export const dummyRegion = new Cairo.Region();
-export const enable_click_through = (self) => self.input_shape_combine_region(dummyRegion);
--- config/ags/modules/misc/textview.ts
+++
@@ -1,3 +0,0 @@
-import Gtk from "gi://Gtk?version=3.0";
-
-export const TextView = Widget.subclass<typeof Gtk.TextView, Gtk.TextView.ConstructorProperties>(Gtk.TextView, "AgsTextView");
