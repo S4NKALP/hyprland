300e790ef1a671b9d9b0d7f14590ecdf925529db
S4NKALP <sankalptharu50028@gmail.com>
2024-12-10 23:28:50 +0545

    fix

--- config/fabric/services/__init__.py
+++ config/fabric/services/__init__.py
@@ -1,12 +1,10 @@
 from .brightness import Brightness
 from .screen_record import ScreenRecorder
-from .weather import WeatherInfo
 from .wifi import NetworkClient, Wifi
 
 __all__ = [
     "Brightness",
     "ScreenRecorder",
-    "WeatherInfo",
     "NetworkClient",
     "Wifi",
 ]
