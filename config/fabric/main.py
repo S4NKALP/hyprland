18e38a9f61939af63c7bb5c520cda4f85bcf0531
S4NKALP <sankalptharu50028@gmail.com>
2025-01-11 08:00:54 +0545

    added styles

--- config/fabric/main.py
+++ config/fabric/main.py
@@ -1,10 +1,10 @@
 import os
 
 import setproctitle
-from loguru import logger
-
 from fabric import Application
 from fabric.utils import get_relative_path, monitor_file
+from loguru import logger
+
 from modules.bar.bar import Bar
 from modules.launcher.launcher import Launcher
 from modules.notification_popup import NotificationPopup
@@ -14,18 +14,34 @@ from services import ScreenRecorder
 logger.disable("fabric.hyprland.widgets")
 
 
+def update_main_css():
+    colors_css_path = f"/home/{os.getlogin()}/.cache/material/colors.css"
+    main_css_path = get_relative_path("styles/main.css")
+
+    if os.path.exists(colors_css_path):
+        logger.info(f"[Main] Updating colors.css path in {main_css_path}")
+        with open(main_css_path, "r+") as css_file:
+            content = css_file.read()
+            updated_content = content.replace("@COLORS_CSS_PATH", colors_css_path)
+            css_file.seek(0)
+            css_file.write(updated_content)
+            css_file.truncate()
+    else:
+        logger.warning(f"[Main] colors.css not found at {colors_css_path}")
+
+
 def apply_style(app: Application):
-    logger.info("[Main] CSS applied")
+    logger.info("[Main] Applying CSS")
     app.set_stylesheet_from_file(get_relative_path("styles/main.css"))
 
 
 if __name__ == "__main__":
+    update_main_css()
     sc = ScreenRecorder()
     bar = Bar()
     osd = OSDContainer()
     notif = NotificationPopup()
     launcher = Launcher()
-    launcher.hide()
 
     app = Application(
         "quickbar",
@@ -35,12 +51,13 @@ if __name__ == "__main__":
     )
     setproctitle.setproctitle("quickbar")
 
+    # Monitor CSS files for changes
     css_file = monitor_file(get_relative_path("styles"))
     _ = css_file.connect("changed", lambda *_: apply_style(app))
 
     color_css_file = monitor_file(f"/home/{os.getlogin()}/.cache/material/colors.css")
     _ = color_css_file.connect("changed", lambda *_: apply_style(app))
 
+    # Apply the styles and run the application
     apply_style(app)
-
     app.run()
