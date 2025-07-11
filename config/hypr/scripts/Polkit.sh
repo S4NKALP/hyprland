af71e9044652b913d29fb06af641a8d0dbe02cc3
S4NKALP <sankalptharu50028@gmail.com>
2025-07-11 07:58:45 +0545

    🔧 chore: update code

--- config/hypr/scripts/Polkit.sh
+++ config/hypr/scripts/Polkit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
 # This is for polkits, it will start from top and will stop if the top is executed
 
 # Polkit possible paths files to check
 polkit=(
   "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+  "/usr/libexec/hyprpolkitagent"
+  "/usr/lib/hyprpolkitagent"
+  "/usr/lib/hyprpolkitagent/hyprpolkitagent"
   "/usr/lib/polkit-kde-authentication-agent-1"
   "/usr/lib/polkit-gnome-authentication-agent-1"
   "/usr/libexec/polkit-gnome-authentication-agent-1"
+  "/usr/libexec/polkit-mate-authentication-agent-1"
   "/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
   "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
 )
