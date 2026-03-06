bdfc04a413da61f33c81adcff56de4675c10974d
S4NKALP <sankalptharu50028@gmail.com>
2026-03-06 21:57:58 +0545

    feat(config/archive): Add initial Zsh configuration and helper utilities
    - New Features
      - Add .zshenv defining ZDOTDIR to ~/.config/zsh and generate an extensive .zcompdump with 1000+ completions
      - Implement _smooth_fzf, _sudo_replace_buffer, _sudo_command_line, and _vi_search_fix helper functions
      - Extend `cd` to automatically list directory contents after change
      - Provide `git-svn` convenience wrapper for exporting SVN subdirectories
    
    - Enhancements
      - Minor refinements to sudo command line completion and editor handling
    
    - Bug Fixes
      - None

---
+++ config/archive/zsh/aliases.zsh
@@ -0,0 +1,110 @@
+
+##
+## Aliases
+##
+
+# Use the same editor
+alias vim='$EDITOR'
+alias vi='$EDITOR'
+alias code='$EDITOR'
+
+# Suffix aliases (execute specific file types with desired tool)
+alias edit='vim'              # Edit files with vim
+
+#List
+alias ls="eza --color=auto --icons"
+alias l="ls -l"
+alias ll="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
+#pacman unlock
+alias unlock="sudo rm /var/lib/pacman/db.lck"
+alias rmpacmanlock="sudo rm /var/lib/pacman/db.lck"
+
+# aur helper
+alias i="paru -S"
+alias r="paru -Rns"
+alias u="paru -Syu"
+alias s="paru -Ss"
+alias Q="paru -Q"
+
+#cache clean
+alias clean="paru -Scc"
+
+# pacman or pm
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu'
+alias upgrade='paru -Syyu'
+
+#add new fonts
+alias update-fc='sudo fc-cache -fv'
+
+#switch between bash and zsh
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+#Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+
+
+#fix obvious typo's
+alias c="clear"
+alias q="exit"
+alias :q="exit"
+alias mtar='tar -zcvf' # mtar <archive_compress>
+alias utar='tar -zxvf' # utar <archive_decompress> <file_list>
+alias z='zip -r' # z <archive_compress> <file_list>
+alias uz='unzip' # uz <archive_decompress> -d <dir>
+alias sr='source ~/.config/zsh/env.zsh'
+alias ..="cd .."
+alias mkdir="mkdir -p"
+alias cat="bat --color always --plain"
+alias grep='grep --color=auto'
+alias mv='mv -v'
+alias cp='cp -vr'
+alias rm='rm -vr'
+
+
+#clean caches
+alias cleanram="sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
+
+#grub update
+alias mkgrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+# other
+alias py='python'
+alias tree='tree -C'
+
+venv() {
+  if [[ -d .venv ]]; then
+    source .venv/bin/activate && echo "Activated existing .venv"
+  else
+    python -m venv .venv && source .venv/bin/activate && echo "Created and activated new .venv"
+  fi
+}
+
+act() {
+  local dir="$PWD"
+  while [[ "$dir" != "/" ]]; do
+    if [[ -f "$dir/venv/bin/activate" ]]; then
+      source "$dir/venv/bin/activate" && echo "Activated: $dir/venv"
+      return
+    elif [[ -f "$dir/.venv/bin/activate" ]]; then
+      source "$dir/.venv/bin/activate" && echo "Activated: $dir/.venv"
+      return
+    fi
+    dir="$(dirname "$dir")"
+  done
+  echo "No virtualenv found"
+}
+
+# Wifi management
+alias wifi-gui='nm-connection-editor'
+alias wifi-tui='nmtui'
+
+# git Aliases
+alias commit="git add . && git commit -m"
+alias push="git push"
+# vim:ft=zsh
---
+++ config/archive/zsh/theme.zsh
@@ -0,0 +1,40 @@
+##
+## Aesthetic Night Colorscheme
+##
+# special
+background='#1A1B26'
+foreground='#C0CAF5'
+
+# black
+color0='#414868'
+color8='#414868'
+
+# red
+color1='#F7768E'
+color9='#F7768E'
+
+# green
+color2='#9ECE6A'
+color10='#9ECE6A'
+
+# yellow
+color3='#E0AF68'
+color11='#E0AF68'
+
+# blue
+color4='#7AA2F7'
+color12='#7AA2F7'
+
+# magenta
+color5='#BB9AF7'
+color13='#BB9AF7'
+
+# cyan
+color6='#7DCFFF'
+color14='#7DCFFF'
+
+# white
+color7='#A9B1D6'
+color15='#C0CAF5'
+
+# vim='iletype'=zsh
