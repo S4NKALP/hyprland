097943ded071fb55f04380afb08576fd1399ceaa
S4NKALP <sankalptharu50028@gmail.com>
2026-05-11 20:37:10 +0545

    fix

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -6,9 +6,5 @@ require("configs.windowrules")
 require("configs.startup_apps")
 require("configs.misc")
 
-
 dofile("/home/sankalp/.config/Modus/config/hypr/modus.lua")
 
-hl.on("hyprland.start", function()
-    hl.exec_cmd("uwsm app -- $(python /home/sankalp/.config/Modus/main.py)")
-end)
