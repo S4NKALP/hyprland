7e951973e3d215c8adc97ce20e8ea2f7c4c00f32
S4NKALP <sankalptharu50028@gmail.com>
2026-07-05 20:00:23 +0545

    chore(config): update Modus theme colors and configuration

---
+++ config/colors.lua
@@ -0,0 +1,187 @@
+return {
+
+    background = "rgb(121318)",
+
+    blue = "rgb(bbc3ff)",
+
+    blue_container = "rgb(3a4379)",
+
+    blue_source = "rgb(0000ff)",
+
+    blue_value = "rgb(0000ff)",
+
+    cyan = "rgb(82d3e2)",
+
+    cyan_container = "rgb(004e59)",
+
+    cyan_source = "rgb(00ffff)",
+
+    cyan_value = "rgb(00ffff)",
+
+    error = "rgb(ffb4ab)",
+
+    error_container = "rgb(93000a)",
+
+    green = "rgb(95d5a7)",
+
+    green_container = "rgb(10512f)",
+
+    green_source = "rgb(00ff00)",
+
+    green_value = "rgb(00ff00)",
+
+    inverse_on_surface = "rgb(2f3036)",
+
+    inverse_primary = "rgb(4f5b92)",
+
+    inverse_surface = "rgb(e3e1e9)",
+
+    magenta = "rgb(e4b7f3)",
+
+    magenta_container = "rgb(5c386b)",
+
+    magenta_source = "rgb(ff00ff)",
+
+    magenta_value = "rgb(ff00ff)",
+
+    on_background = "rgb(e3e1e9)",
+
+    on_blue = "rgb(232c61)",
+
+    on_blue_container = "rgb(dfe0ff)",
+
+    on_cyan = "rgb(00363e)",
+
+    on_cyan_container = "rgb(9eefff)",
+
+    on_error = "rgb(690005)",
+
+    on_error_container = "rgb(ffdad6)",
+
+    on_green = "rgb(00391d)",
+
+    on_green_container = "rgb(b0f1c2)",
+
+    on_magenta = "rgb(442253)",
+
+    on_magenta_container = "rgb(f7d8ff)",
+
+    on_primary = "rgb(1f2c61)",
+
+    on_primary_container = "rgb(dde1ff)",
+
+    on_primary_fixed = "rgb(07164b)",
+
+    on_primary_fixed_variant = "rgb(374379)",
+
+    on_red = "rgb(561d26)",
+
+    on_red_container = "rgb(ffdadc)",
+
+    on_secondary = "rgb(2c2f42)",
+
+    on_secondary_container = "rgb(dfe1f9)",
+
+    on_secondary_fixed = "rgb(171b2c)",
+
+    on_secondary_fixed_variant = "rgb(424659)",
+
+    on_surface = "rgb(e3e1e9)",
+
+    on_surface_variant = "rgb(c6c5d0)",
+
+    on_tertiary = "rgb(44273f)",
+
+    on_tertiary_container = "rgb(ffd7f4)",
+
+    on_tertiary_fixed = "rgb(2c1229)",
+
+    on_tertiary_fixed_variant = "rgb(5c3d56)",
+
+    on_white = "rgb(00363d)",
+
+    on_white_container = "rgb(9eeffd)",
+
+    on_yellow = "rgb(263500)",
+
+    on_yellow_container = "rgb(d4ec9d)",
+
+    outline = "rgb(90909a)",
+
+    outline_variant = "rgb(45464f)",
+
+    primary = "rgb(b8c4ff)",
+
+    primary_container = "rgb(374379)",
+
+    primary_fixed = "rgb(dde1ff)",
+
+    primary_fixed_dim = "rgb(b8c4ff)",
+
+    red = "rgb(ffb2b9)",
+
+    red_container = "rgb(72333b)",
+
+    red_source = "rgb(ff0000)",
+
+    red_value = "rgb(ff0000)",
+
+    scrim = "rgb(000000)",
+
+    secondary = "rgb(c2c5dd)",
+
+    secondary_container = "rgb(424659)",
+
+    secondary_fixed = "rgb(dfe1f9)",
+
+    secondary_fixed_dim = "rgb(c2c5dd)",
+
+    shadow = "rgb(000000)",
+
+    source_color = "rgb(6280fa)",
+
+    surface = "rgb(121318)",
+
+    surface_bright = "rgb(38393f)",
+
+    surface_container = "rgb(1f1f25)",
+
+    surface_container_high = "rgb(292a2f)",
+
+    surface_container_highest = "rgb(34343a)",
+
+    surface_container_low = "rgb(1b1b21)",
+
+    surface_container_lowest = "rgb(0d0e13)",
+
+    surface_dim = "rgb(121318)",
+
+    surface_tint = "rgb(b8c4ff)",
+
+    surface_variant = "rgb(45464f)",
+
+    tertiary = "rgb(e4bad9)",
+
+    tertiary_container = "rgb(5c3d56)",
+
+    tertiary_fixed = "rgb(ffd7f4)",
+
+    tertiary_fixed_dim = "rgb(e4bad9)",
+
+    white = "rgb(82d3e0)",
+
+    white_container = "rgb(004f58)",
+
+    white_source = "rgb(ffffff)",
+
+    white_value = "rgb(ffffff)",
+
+    yellow = "rgb(b8cf84)",
+
+    yellow_container = "rgb(3b4d0f)",
+
+    yellow_source = "rgb(ffff00)",
+
+    yellow_value = "rgb(ffff00)",
+
+}
